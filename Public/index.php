758f4bd67f2d63bec37536cc4e66cd892c4d13ba
mohamedredachakir <chakirm082@gmail.com>
2026-01-01 20:54:28 +0100

    change strecture of all tables in schema.sql

---
+++ Public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ .'/../bootstrap/autoload.php';
+
+use App\Core\router;
+
+
+session_start();
+
+$router = new Router();
--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 
 
 
-CREATE DATABASE khatriii
+CREATE DATABASE KHATRiii
 
     CREATE TABLE users (
         id INT AUTO_ENCREMENT PRIMARY KEY,
@@ -9,29 +9,34 @@ CREATE DATABASE khatriii
         last_name VARCHAR(50) NOT NULL,
         email VARCHAR(50) NOT NULL UNIQUE,
         passowrd VARCHAR(255) NOT NULL,
+        role ENUM('reader','author','admin') NOT NULL DEFAULT 'reader',
         active BOOLEAN DEFAULT FALSE,
         count_report INT DEFAULT 0,
+        start_report DATETIME,
+        end_report DATETIME,
         create_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    );
-
-    CREATE TABLE roles (
-        id_user INT NOT NULL UNIQUE,
-        first_name VARCHAR(50),
-        role ENUM('reader','author','admin') NOT NULL DEFAULT 'reader',
-        FOREIGN KEY (id_user) REFERENCES users(id),
-        FOREIGN KEY (first_name) REFERENCES users(first_name)
     );
 
     CREATE TABLE articles (
         id INT AUTO_ENCREMENT PRIMARY KEY,
         id_user INT NOT NULL,
         title VARCHAR(100) NOT NULL,
-        name_author VARCHAR(50) NOT NULL,
         content LONGTEXT NOT NULL,
         create_at DATETIME NOT NULL,
-        category ENUM('history','philosophy','science','other') NOT NULL,
-        FOREIGN KEY (id_user) REFERENCES users(id),
-        FOREIGN KEY (name_author) REFERENCES users(first_name)
+        FOREIGN KEY (id_user) REFERENCES users(id)
+    );
+
+    CREATE TABLE categories (
+        id INT AUTO_ENCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL
+    );
+
+    CREATE TABLE article_categorie (
+        id_article INT NOT NULL,
+        id_categorie INT NOT NULL,
+        PRIMARY KEY (id_article,id_categorie),
+        FOREIGN KEY (id_article) REFERENCES articles (id),
+        FOREIGN KEY (id_categorie) REFERENCES categories (id)
     );
 
     CREATE TABLE commentaires (
@@ -45,20 +50,25 @@ CREATE DATABASE khatriii
     );
 
     CREATE TABLE like_article (
-        id INT AUTO_ENCREMENT PRIMARY KEY,
         id_article INT NOT NULL,
         id_reader INT NOT NULL,
+        PRIMARY KEY (id_article,id_reader),
         FOREIGN KEY (id_article) REFERENCES articles (id),
         FOREIGN KEY (id_reader) REFERENCES users (id)
     );
 
     CREATE TABLE like_commentaire (
-        id INT AUTO_ENCREMENT PRIMARY KEY,
         id_reader INT NOT NULL,
-        id_article NOT NULL,
-        id_commentaire NOT NULL,
+        id_article INT NOT NULL,
+        id_commentaire INT NOT NULL,
+        PRIMARY KEY (id_commentaire, id_reader),
         FOREIGN KEY (id_reader) REFERENCES users(id),
         FOREIGN KEY (id_article) REFERENCES articles(id),
         FOREIGN KEY (id_commentaire) REFERENCES commentaires(id)
     );
 
+
+    --     history 
+    --     philosophy 
+    --     science 
+    --     other 
