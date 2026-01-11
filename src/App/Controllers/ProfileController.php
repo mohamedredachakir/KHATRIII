858fcb463bd92f82e8ae15527cdd86ca37c36973
reddcs <chakirm082@gmail.com>
2026-01-11 13:01:49 +0100

    create methode beauthor in profilecontroller

--- src/App/Controllers/ProfileController.php
+++ src/App/Controllers/ProfileController.php
@@ -12,43 +12,40 @@ class ProfileController {
         };
     }
 
-    private function checkauthor() {
-        if($_SESSION['user']['role'] !== 'author'){
-            header('Location: /');
-            exit();
-        };
-    }
-
-    private function checkreader() {
-        if($_SESSION['user']['role'] !== 'reader'){
-            header('Location: /');
-            exit();
-        };
-    }
-
-
-    private function checkadmin() {
-        if($_SESSION['user']['role'] !== 'admin'){
-            header('Location: /');
-            exit(); 
-        };
-    }
-
-    public function showreaderprofile() {
+    private function beauthor() {
         $this->checkauth();
-        
-        require_once __DIR__ .'/../Views/Profile/profilereader.php';
-
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+            header('Location: /profile');
+            exit();
+        }
+        if($_SESSION['user']['role'] !== 'reader'){
+            header('Location: /profile');
+            exit();
+        }
+        $conn = Database::getconnection();
+        $id = $_SESSION['user']['id'];
+        $role = 'author';
+        $sql = 'UPDATE users SET role = ? WHERE id = ?';
+        $stmt = $conn->prepare($sql);
+        if($stmt->execute([$role,$id])){
+            header('Location: /profile');
+            exit();
+        };
     }
 
-    // public function showauthorprofile() {
-    //     $this->checkauthor();
-        
-    //     require_once __DIR__ .'/../Views/Profile/profile.php';
-    // }
-    // public function showadminprofile() {
-    //     $this->checkauth();
-        
-    //     require_once __DIR__ .'/../Views/Profile/profile.php';
-    // }
+    public function showprofile() {
+        $this->checkauth();
+        if($_SESSION['user']['role'] === 'author'){
+            require_once __DIR__ .'/../Views/Profile/profileauthor.php';
+        };
+         if($_SESSION['user']['role'] === 'reader'){
+            require_once __DIR__ .'/../Views/Profile/profilereader.php';
+        };
+         if($_SESSION['user']['role'] === 'admin'){
+            require_once __DIR__ .'/../Views/Profile/dashboard.php';
+        };
+    }
+    
+
+
 }
